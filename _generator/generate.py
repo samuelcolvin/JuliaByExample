b2612849dde716dfacb139f21370a7e7e026b97d
samuelcolvin <s@muelcolvin.com>
2014-02-12 15:56:15 +0000

    added julia source examples and worked on page generation

--- _generator/generate.py
+++ _generator/generate.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 
-import json, os, urllib2, jinja2, shutil, markdown2
+import json, os, urllib2, jinja2, shutil, markdown2, sys, re
 from copy import copy
+from jinja2 import contextfunction
+from pygments import highlight
+import pygments.lexers as pyg_lexers
+from pygments.formatters import HtmlFormatter
 
 THIS_PATH = os.path.dirname(os.path.realpath(__file__))
 PROJ_ROOT = os.path.realpath(os.path.join(THIS_PATH, os.pardir))
@@ -9,6 +13,16 @@ STATIC_PATH = os.path.join(THIS_PATH, 'static')
 WWW_PATH = os.path.join(PROJ_ROOT, 'www')
 WWW_STATIC_PATH = os.path.join(WWW_PATH, 'static')
 TEMPLATE_PATH = os.path.join(THIS_PATH, 'templates')
+PAGES = {
+  'Common Usage': {
+    'page': 'index',
+    'source': 'common_usage'
+  },
+  'Source Examples': {
+    'page': 'julia_source',
+    'source': 'julia_source_examples'
+  }
+}
 
 BASIC_CONTEXT = {
 'title': 'Julia By Example',
@@ -19,22 +33,28 @@ BASIC_CONTEXT = {
 'root_url': 'index.html',
 }
 
+@contextfunction
+def code_file(context, file_name, **extra_context):
+    file_path = os.path.join(context['example_directory'], file_name)
+    file_text = open(file_path, 'r').read()
+    lexer = pyg_lexers.get_lexer_for_filename(file_name)
+    formatter = HtmlFormatter(cssclass='code')#linenos=True, 
+    return highlight(file_text, lexer, formatter)
+
 class SiteGenerator(object):
     def __init__(self, output = None):
         self._env = jinja2.Environment(loader= jinja2.FileSystemLoader(TEMPLATE_PATH))
         if output:
             self._output = output
         self.delete_www()
-        pages_file = os.path.join(PROJ_ROOT, 'pages.json')
-        pages = json.load(open(pages_file, 'r'))
-        self.base_context(pages)
-        for info in pages.values():
+        self.base_context()
+        for info in PAGES.values():
             self.generate_page(info)
-        self.move_statics()
+        self.generate_statics()
         
-    def base_context(self, pages):
+    def base_context(self):
         ex_pages = []
-        for title, info in pages.items():
+        for title, info in PAGES.items():
             ex_pages.append({'url':info['page'] + '.html', 'title': title})
         self.context = BASIC_CONTEXT
         self.context['example_pages'] = ex_pages
@@ -45,11 +65,26 @@ class SiteGenerator(object):
     def generate_page(self, info):
         template = 'examples.template.html'
         self._template = self._env.get_template(template)
-        descrip_file = os.path.join(PROJ_ROOT, info['source'], 'description.md')
-        description = open(descrip_file, 'r').read()
+        example_dir = os.path.join(PROJ_ROOT, info['source'])
+        ex_env = jinja2.Environment(loader= jinja2.FileSystemLoader(example_dir))
+        ex_env.globals['code_file'] = code_file
+        ex_template = ex_env.get_template('description.md')
+        
+        description = ex_template.render(example_directory = example_dir)
+        description = markdown2.markdown(description)
+        tagno = 0
+        while re.search('<[hH][123456]>', description):
+            tagno += 1
+            description = re.sub('<([hH])([123456])>', r'<\1\2 id="tag%d">' % tagno, description, count=1)
+        tags = []
+        for g in re.finditer('<[hH][123456] id="(.*?)">(.*?)</[hH]', description):
+            parts = g.groups()
+            tags.append({'link': '#' + parts[0], 'name': parts[1]})
+        
         new_context = copy(self.context)
-        new_context['examples'] = markdown2.markdown(description)
+        new_context['examples'] = description
         new_context['page'] = '%s.html' % info['page']
+        new_context['tags'] = tags
         page_text = self._template.render(**new_context)
         page_path = os.path.join(WWW_PATH, '%s.html' % info['page'])
         open(page_path, 'w').write(page_text)
@@ -58,39 +93,28 @@ class SiteGenerator(object):
             fn2 = '...%s' % fn2[-37:]
         self._output('generated html file "%s" from page: %s, using template: %s' % (fn2, info['page'], template))
     
-#     def generate_page(self, page):
-#         context = {}
-#         for var, item in page['context'].items():
-#             context[var] = item['value']
-#             if item['type'] == 'markdown':
-#                 context[var] = markdown2.markdown(item['value'])
-#         r = tr.RenderTemplate(page['template'])
-#         content = r.render(context)
-#         fn = os.path.join(WWW_PATH, '%s.html' % page['name'])
-#         open(fn, 'w').write(content)
-#         os.chmod(fn, 0666)
-#         fn2 = fn
-#         if len(fn2) > 40:
-#             fn2 = '...%s' % fn2[-37:]
-#         self._output('generated html file "%s" from page: %s, using template: %s' % (fn2, page['name'], page['template']))
-        
-    def move_statics(self):
+    def generate_statics(self):
         if os.path.exists(STATIC_PATH):
             shutil.copytree(STATIC_PATH, WWW_STATIC_PATH)
             self._output('copied static files')
         down_success = self.download_libraries()
         if not down_success:
             raise Exception('Error downloading libraries')
+        self.generate_pyg_css()
         
     def delete_www(self):
         if os.path.exists(WWW_PATH):
             shutil.rmtree(WWW_PATH)
             self._output('deleting existing site: %s' % WWW_PATH)
         os.makedirs(WWW_PATH)
-
+        
+    def generate_pyg_css(self):
+        pyg_css = HtmlFormatter().get_style_defs('.code')
+        file_path = os.path.join(WWW_STATIC_PATH, 'pygments.css')
+        open(file_path, 'w').write(pyg_css)
 
     def download_libraries(self):
-        libs_json_path = 'libraries.json'
+        libs_json_path = os.path.join(THIS_PATH, 'libraries.json')
         url_files = json.load(open(libs_json_path, 'r'))
         downloaded = 0
         ignored = 0
@@ -122,6 +146,12 @@ class SiteGenerator(object):
 
     def _output(self, msg):
         print msg
+
+def list_examples_by_size(examples_dir = 'julia_source_examples'):
+    path = os.path.join(PROJ_ROOT, examples_dir)
+    files = [(os.path.getsize(os.path.join(path, fn)), fn) for fn in os.listdir(path)]
+    files.sort()
+    print ''. join(['\n\n#### %s\n\n{{ code_file(\'%s\') }} ' % (fn, fn) for _, fn in files if fn.endswith('.jl')])
 
 if __name__ == '__main__':
     SiteGenerator()
---
+++ julia_source_examples/description.md
@@ -0,0 +1,83 @@
+#### quine.jl
+
+{{ code_file('quine.jl') }} 
+
+#### bubblesort.jl
+
+{{ code_file('bubblesort.jl') }} 
+
+#### enum.jl
+
+{{ code_file('enum.jl') }} 
+
+#### modint.jl
+
+{{ code_file('modint.jl') }} 
+
+#### queue.jl
+
+{{ code_file('queue.jl') }} 
+
+#### queens.jl
+
+{{ code_file('queens.jl') }} 
+
+#### preduce.jl
+
+{{ code_file('preduce.jl') }} 
+
+#### time.jl
+
+{{ code_file('time.jl') }} 
+
+#### ndgrid.jl
+
+{{ code_file('ndgrid.jl') }} 
+
+#### lru_test.jl
+
+{{ code_file('lru_test.jl') }} 
+
+#### ndrange.jl
+
+{{ code_file('ndrange.jl') }} 
+
+#### staged.jl
+
+{{ code_file('staged.jl') }} 
+
+#### list.jl
+
+{{ code_file('list.jl') }} 
+
+#### tree.jl
+
+{{ code_file('tree.jl') }} 
+
+#### plife.jl
+
+{{ code_file('plife.jl') }} 
+
+#### quaternion.jl
+
+{{ code_file('quaternion.jl') }} 
+
+#### wordcount.jl
+
+{{ code_file('wordcount.jl') }} 
+
+#### lru.jl
+
+{{ code_file('lru.jl') }} 
+
+#### typetree.jl
+
+{{ code_file('typetree.jl') }} 
+
+#### hpl.jl
+
+{{ code_file('hpl.jl') }} 
+
+#### colorimetry.jl
+
+{{ code_file('colorimetry.jl') }} 
