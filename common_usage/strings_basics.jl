c6a2d76a7ae8c11cb0b9fb4b0eb866cf3bc7daa9
Samuel Colvin <s@muelcolvin.com>
2014-07-03 13:55:10 +0100

    add string concat examples

--- common_usage/strings_basics.jl
+++ common_usage/strings_basics.jl
@@ -2,13 +2,16 @@
 # like variables, strings can contain any unicode character
 s1 = "The quick brown fox jumps over the lazy dog α,β,γ"
 println(s1)
+#> The quick brown fox jumps over the lazy dog α,β,γ
 
 # [println](http://julia.readthedocs.org/en/latest/stdlib/base/#Base.println) adds a new line to the end of output
 # [print](http://julia.readthedocs.org/en/latest/stdlib/base/#Base.print) can be used if you dont want that:
 print("this")
+#> this
 print(" and")
+#> and
 print(" that.\n")
-#> this and that.
+#> that.
 
 # chars are defined with single quotes
 c1 = 'a'
@@ -28,6 +31,8 @@ println(int('1') == 1)
 s1_caps = uppercase(s1)
 s1_lower = lowercase(s1)
 println(s1_caps, "\n", s1_lower)
+#> THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG Α,Β,Γ
+#> the quick brown fox jumps over the lazy dog α,β,γ
 
 # sub strings can be indexed like arrays:
 # ([show](http://julia.readthedocs.org/en/latest/stdlib/base/#Base.show) prints the raw value)
@@ -46,13 +51,20 @@ show(s1[end-10:end]); println()
 a = "wolcome"
 b = "julia"
 println("$a to $b.")
-#> welcome to julia.
+#> wolcome to julia.
 
 # this can extend to evaluate statements:
 println("1 + 2 = $(1 + 2)")
 #> 1 + 2 = 3
 
-# strings can also be concatenated using the string function
-s2 = string("this", " and", " that")
+# strings can also be concatenated using the * operator
+# using * instead of + isn't intuitive when you start with Julia,
+# however [people think it makes more sense](https://groups.google.com/forum/#!msg/julia-users/nQg_d_n0t1Q/9PSt5aya5TsJ)
+s2 = "this" * " and" * " that"
 println(s2)
+#> this and that
+
+# as well as the string function
+s3 = string("this", " and", " that")
+println(s3)
 #> this and that
