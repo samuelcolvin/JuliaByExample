74afa4f19581b90ae684993c8181357d9398014d
Samuel Colvin <s@muelcolvin.com>
2016-04-27 13:33:34 +0000

    Update documentation

--- src/dicts.jl
+++ src/dicts.jl
@@ -22,15 +22,17 @@ a3 = Dict([i => @sprintf("%d", i) for i = 1:10])
 printsum(a3)#> Dict{Any,Any}: {5=>"5",4=>"4",6=>"6",7=>"7",2=>"2",10=>"10",9=>"9",8=>"8",3=>"3",1=>"1"}
 
 # as you would expect, Julia comes with all the normal helper functions
-# for dicts, e.g., (haskey)[http://docs.julialang.org/en/latest/stdlib/base/#Base.haskey]
+# for dicts, e.g., [haskey](http://docs.julialang.org/en/latest/stdlib/base/#Base.haskey)
 println(haskey(a1,1)) #> true
 
 # which is equivalent to
 println(1 in keys(a1)) #> true
 # where [keys](http://docs.julialang.org/en/latest/stdlib/base/#Base.keys) creates an iterator over the keys of the dictionary
 
-# similar to keys, (values)[http://docs.julialang.org/en/latest/stdlib/base/#Base.values] get iterators over the dict's values:
-printsum(values(a1)) #> ValueIterator{Dict{Any,Any}}: ValueIterator{Dict{Any,Any}}({2=>"two",3=>"three",1=>"one"})
+# similar to keys, [values](http://docs.julialang.org/en/latest/stdlib/base/#Base.values) get iterators over the dict's values:
+printsum(values(a1)) 
+#> Base.ValueIterator for a Dict{Int64,ASCIIString} with 3 entries: ASCIIString["two","three","one"]
 
 # use [collect](http://docs.julialang.org/en/latest/stdlib/base/#Base.collect) to get an array:
-printsum(collect(values(a1)))#> 3-element Array{Any,1}: {"two","three","one"}
+printsum(collect(values(a1)))
+#> 3-element Array{ASCIIString,1}: ASCIIString["two","three","one"]
