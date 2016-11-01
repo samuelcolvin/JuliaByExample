36546703c38bba46683ab11133724a3a2fe63215
Samuel Colvin <s@muelcolvin.com>
2016-11-01 19:04:14 +0000

    switch index to redirect

--- src/error_handling.jl
+++
@@ -1,8 +0,0 @@
-a=[]
-# [try, catch](http://julia.readthedocs.org/en/latest/manual/control-flow/#the-try-catch-statement) can be used to deal with errors as with many other languages
-try
-    push!(a,1)
-catch err
-    showerror(STDOUT, err, backtrace());println()
-end
-println("Continuing after error")
